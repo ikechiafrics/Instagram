a52aea369531047c4df289c6ab5d4cab7acb1e53
ikechiafrics <93938295+ikechiafrics@users.noreply.github.com>
2022-06-17 17:32:44 -0700

    Added styles Added a Bottom Navigation Vew Implemented fragments in each menu Added non-functioning like and comment button Added the profile picture of each users made the like button functional added a toolbar as the actionbar added app icon to home screen Added Sign up feature

--- app/src/main/java/com/example/instagram/LoginActivity.java
+++ app/src/main/java/com/example/instagram/LoginActivity.java
@@ -20,6 +20,7 @@ public class LoginActivity extends AppCompatActivity {
     private EditText etUsername;
     private EditText etPassword;
     private Button btnLogin;
+    private Button btnSignup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +35,7 @@ public class LoginActivity extends AppCompatActivity {
         etUsername = findViewById(R.id.etUsername);
         etPassword = findViewById(R.id.etPassword);
         btnLogin = findViewById(R.id.btnLogin);
+        btnSignup = findViewById(R.id.btnSignup);
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -41,6 +43,13 @@ public class LoginActivity extends AppCompatActivity {
                 String username = etUsername.getText().toString();
                 String password = etPassword.getText().toString();
                 loginUser(username, password);
+            }
+        });
+
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToSignupActivity();
             }
         });
     }
@@ -67,5 +76,11 @@ public class LoginActivity extends AppCompatActivity {
     startActivity(i);
     finish();
     }
+
+    private void goToSignupActivity() {
+        Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
+        startActivity(intent);
+    }
 }
+
 
